8c9bc1a9ade03f734e91503b55db62f0e2bec842
humonnom <juepark42@gmail.com>
2023-04-10 09:05:11 +0900

    page on time sending하는 코드 추가

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -8,6 +8,39 @@ const GoogleAnalytics = ({ children }) => {
 
   React.useEffect(() => {
     //TODO: 타이밍 트래킹
+    (function () {
+      var startTime = new Date().getTime();
+      var lastSentTime = startTime;
+
+      function sendTimeOnPage() {
+        var currentTime = new Date().getTime();
+        var timeOnPage = Math.round((currentTime - lastSentTime) / 1000);
+
+        if (timeOnPage > 0 && typeof gtag !== "undefined") {
+          ReactGA.gtag("event", "time_on_page", {
+            value: timeOnPage,
+          });
+        }
+
+        lastSentTime = currentTime;
+      }
+
+      window.addEventListener("visibilitychange", function () {
+        if (document.visibilityState === "hidden") {
+          sendTimeOnPage();
+        }
+      });
+
+      window.setInterval(function () {
+        if (document.visibilityState === "visible") {
+          sendTimeOnPage();
+        }
+      }, 10000); // Send time on page every 10 seconds
+
+      window.addEventListener("beforeunload", function () {
+        sendTimeOnPage();
+      });
+    })();
   }, [location]);
 
   return <>{children}</>;
