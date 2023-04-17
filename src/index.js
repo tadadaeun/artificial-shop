b1d8e072c6310e7d3628b72a381c268c8ba4c34d
humonnom <juepark42@gmail.com>
2023-04-17 21:07:52 +0900

    add amplitude package and init instance

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import ReactGA from "react-ga4";
+import amplitude from "amplitude-js";
 
+amplitude.getInstance().init("4263e426732edad34ab6ae666a36cc39");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
