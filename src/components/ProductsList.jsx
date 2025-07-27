83fa97393bbd2603d1aa871d0e97986ebe561e62
Seoungwoo Lee <sl2986@gmail.com>
2025-07-27 23:38:27 +0900

    20250727_원래대로

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -68,8 +68,7 @@ const ProductsList = () => {
     else if (sort === "highest") return b.price - a.price;
     else if (sort === "nutrient") {
       // TODO - 오브젝트 매핑추가!
-      // return b.nut > a.nut;
-      return b.nutriScore - a.nutnutriScore;
+      return b.nut > a.nut;
     }
   });
 
@@ -94,8 +93,7 @@ const ProductsList = () => {
           <FilterOptions
             name="sort"
             onChange={(e) => setSort(e.target.value)}
-            // defaultValue="lowest"
-            defaultValue="nutrient"
+            defaultValue="lowest"
           >
             <FilterOption value="default">select option</FilterOption>
             <FilterOption value="lowest">lowest price</FilterOption>
